ff3b43b8929a78408d4c834ade3703d2e6c0b6bd
Viet Tran <viettranx@gmail.com>
2020-11-03 15:59:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,72 @@ const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
+const app_common = {
+  db: null,
+};
+
 
 const PORT = process.env.PORT || 3000;
+
+// PORT=5000 node index.js // -e PORT=5000
+// mongodb+srv://dbUser:dbUserPass@cluster0.ohymx.mongodb.net/todo_app_express?retryWrites=true&w=majority
+
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// URL Convention Restful API
+// GET /todos?page=1&limit=10 (list todo objects)
+// GET /todos/:id (get a todo object by id)
+// PUT /todos/:id (update a todo object by id)
+// DELETE /todos/:id (delete a todo object by id)
+// POST /todos (create a new todo)
+// CRUD APIs
+
+// Ex: POST /users/:id/like
+// Ex: PUT /cart/payment
+// Ex: // GET /todos/:id/comments (get comments of a todo object)
+
+// Handlers/modules/todo/index.js
+// Handlers/modules/todo/create.js
+// Handlers/modules/todo/update.js
+
+
+// Client Web (SEO) <--html--->  Web Server (Redering Side) <--- data json ---> API
+// Client Mobile <--- data json ---> API
+// Client Web (ReactJS / VueJS) PWA  <--- data json ---> API
+
+// JSON Format
+/*
+res.status(400).json(err obj)
+Error:
+{
+    "code": "ErrAccessTokenInactivated",
+    "log": "Token is inactive because it is malformed, expired or otherwise invalid",
+    "status_code": 401,
+    "message": "access token is disabled"
+}
+
+Success:
+{
+    "code": 200,
+    "data": Object | Array
+    "paging": {
+      "page": 1,
+      "limit": 10,
+      "total": 100,
+    }
+}
+*/
+
+// Next step:
+// 1. Use mysql
+// 2. Todo, Category, User (JWT, avatar)
+// 3. Restful API
+// 4. Files & Folders
+
 // Connect MongoDB Cloud
-const uri = `mongodb+srv://dbUser:dbUserPass@cluster0.ohymx.mongodb.net/todo_app_express?retryWrites=true&w=majority`;
+const uri = process.env.DBURL;
 const client = new MongoClient(uri, { useUnifiedTopology: true });
 client.connect(err => {
   const db = client.db("todo_app_express").collection("todos");  
@@ -77,3 +135,10 @@ client.connect(err => {
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
+
+
+// Learning process:
+// Try it (tutorial)
+// Learn it -> research it
+// Looking for References
+// Remake (Improve/Refactor it)
